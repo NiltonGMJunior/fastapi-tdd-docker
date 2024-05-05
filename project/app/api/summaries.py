cf98af16617cfe14af6ce3e3d77e89ba14f1fca5
Nilton Junior <nilton@Niltons-MacBook-Pro.local>
2024-05-05 11:51:01 -0300

    chore: add flake8 and linting

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -15,7 +15,7 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
     summary_id = await crud.post(payload)
 
     response_object = {
-        "id": summary_id, 
+        "id": summary_id,
         "url": payload.url
     }
     return response_object
